f97527f191ffdc4492bcc48d34e4f630f6427f79
JShao <shaojianab@qq.com>
2020-04-10 14:51:09 +0800

    Update preprocess.R

--- R/preprocess.R
+++ R/preprocess.R
@@ -2,17 +2,18 @@
 # Copyright (c) 2020 UCAS
 
 #Use manual format
-prepro <- function(inputdir="", outputdir="", outlierdet = TRUE, interval = 15, imputation = FALSE, 
+prepro <- function(inputdir="", outputdir="", outlierdet = TRUE, interval = 15, imputation = FALSE,
                    immethod = "linear", maxgap = 60, compeleteday = TRUE, removeday = FALSE){
 	fileNames = list.files(inputdir)
 	for(f in fileNames){
 	  print(paste("processing file:", f))
-	  cgmts <- read.csv(paste(inputdir, "/", f, sep = ''))
+	  cgmts <- read.csv(paste(inputdir, f, sep = ''))
 	  colnm <- colnames(cgmts)
-	  if(colnm[1] != "timestmap" || colnm[2] != "sglucose" || colnm[3] != "bglucose"){
+	  if(colnm[1] != "timestamp" || colnm[2] != "sglucose" || colnm[3] != "bglucose"){
 	    stop(paste("The format fo file '",f ,"' is incorrect and cannot be read.",sep = ""))
 	  }
 	  cgmts  = qcfun(cgmts, outlierdet, interval, imputation,immethod, maxgap, compeleteday,removeday)
-	  write.csv(cgmts, paste(outputdir,"/",f,sep=''),row.names = FALSE)
+	  write.csv(cgmts, paste(outputdir,f,sep=''),row.names = FALSE)
 	  }
 }
+
