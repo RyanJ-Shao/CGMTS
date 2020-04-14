f3242ad4432fd499fe42ebc099295288affb760c
JShao <shaojianab@qq.com>
2020-04-14 14:27:53 +0800

    Update qcfun.R

--- R/qcfun.R
+++ R/qcfun.R
@@ -1,16 +1,12 @@
 # The MIT License (MIT)
 # Copyright (c) 2020 UCAS
-library(dplyr)
-library(imputeTS)
-library(forecast)
-library(TSA)
 qcfun<- function(cgmts, outlierdet = TRUE, interval = 15, imputation = FALSE, immethod = "linear",
                  maxgap = 60, compeleteday = TRUE, removeday = FALSE, transunits = FALSE){
   vectimestamp <- as.vector(cgmts$timestamp)
   vectimestamp <- unlist(strsplit(vectimestamp,split=" "))
   maxtimestamp <- matrix(vectimestamp,ncol=2,byrow=T)[,1]
-  cgmts <- cgmts %>% mutate(timedate = maxtimestamp)
-  coldate <- unique(cgmts$timedate)
+  cgmts <-  dplyr::mutate(cgmts, timedate = maxtimestamp)
+  coldate <- dplyr::unique(cgmts$timedate)
   freq = 1440/interval
   #remove first day and last day
   fday <- coldate[1]
@@ -24,7 +20,7 @@ qcfun<- function(cgmts, outlierdet = TRUE, interval = 15, imputation = FALSE, im
   }
   cgmts <- cgmts[!is.na(cgmts$timedate),]
   if(transunits){
-    cgmts <- mutate(cgmts, sglucose = round(sglucose/18,2))
+    cgmts <- dplyr::mutate(cgmts, sglucose = round(sglucose/18,2))
   }
 
   if(compeleteday){
@@ -37,22 +33,22 @@ qcfun<- function(cgmts, outlierdet = TRUE, interval = 15, imputation = FALSE, im
   }else if(imputation){
     if(immethod == "linear"){
       print("linear imputation")
-      cgmts <- cgmts %>% mutate(imglucose = na.interpolation(cgmts$sglucose,maxgap = as.integer(maxgap/interval)))
+      cgmts <- dplyr::mutate(cgmts, imglucose = na.interpolation(cgmts$sglucose,maxgap = as.integer(maxgap/interval)))
     }else if(immethod == "seadec"){
       print("SEADEC imputation")
       tstype <- ts(cgmts$sglucose,frequency = freq)
-      tstype <- na.seadec(tstype,algorithm = "interpolation",maxgap = as.integer(maxgap/interval))
-      cgmts <- cgmts %>% mutate(imglucose = tstype)
+      tstype <- imputeTS::na.seadec(tstype,algorithm = "interpolation",maxgap = as.integer(maxgap/interval))
+      cgmts <- dplyr::mutate(cgmts, imglucose = tstype)
     }else if(immethod == "arima"){
       print("ARIMA imputation")
-      cgmts <- cgmts %>% mutate(imglucose = na.kalman(cgmts$sglucose,model = "auto.arima",maxgap = as.integer(maxgap/interval)))
+      cgmts <- dplyr::mutate(cgmts, imglucose = na.kalman(cgmts$sglucose,model = "auto.arima",maxgap = as.integer(maxgap/interval)))
 
     }
     if(removeday == TRUE){
       is.na.rle <- rle(is.na(cgmts$sglucose))
       is.na.rle$values <- is.na.rle$values & is.na.rle$lengths > as.integer(maxgap/interval)
       reov = cgmts[inverse.rle(is.na.rle), ]
-      reovdate = unique(reov$timedate)
+      reovdate = dplyr::unique(reov$timedate)
       for (d in reovdate){
         cgmts[cgmts$timedate ==d,]$timedate <- NA
       }
@@ -63,7 +59,7 @@ qcfun<- function(cgmts, outlierdet = TRUE, interval = 15, imputation = FALSE, im
       is.na.rle <- rle(is.na(cgmts$sglucose))
       is.na.rle$values <- is.na.rle$values & is.na.rle$lengths > as.integer(maxgap/interval)
       reov = cgmts[inverse.rle(is.na.rle), ]
-      reovdate = unique(reov$timedate)
+      reovdate = dplyr::unique(reov$timedate)
       for (d in reovdate){
         cgmts[cgmts$timedate ==d,]$timedate <- NA
       }
@@ -72,16 +68,16 @@ qcfun<- function(cgmts, outlierdet = TRUE, interval = 15, imputation = FALSE, im
 
   }
   if(outlierdet == TRUE){
-    cgmts <- mutate(cgmts, outliers = NA)
-    unidate = unique(cgmts$timedate)
+    cgmts <- dplyr::mutate(cgmts, outliers = NA)
+    unidate = dplyr::unique(cgmts$timedate)
     for (d in unidate){
       udcgm <- cgmts[cgmts$timedate ==d,]
       if(!any(is.na(udcgm$sglucose))){
         udcgmglucose <- udcgm$sglucose
         udcgmts <- ts(udcgmglucose, frequency = freq)
-        tsmodel <- auto.arima(udcgmts)
-        ao <- detectAO(tsmodel)
-        io <- detectIO(tsmodel)
+        tsmodel <- forecast::auto.arima(udcgmts)
+        ao <- TSA::detectAO(tsmodel)
+        io <- TSA::detectIO(tsmodel)
         aodict <- c(ao$lambda2)
         names(aodict) <- ao$ind
 
