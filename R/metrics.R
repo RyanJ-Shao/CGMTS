dbb6525e6c475a20d82f78f2496be1505d0ba1a9
JShao <shaojianab@qq.com>
2020-05-13 09:52:04 +0800

    Update metrics.R

--- R/metrics.R
+++ R/metrics.R
@@ -1,55 +1,375 @@
 # The MIT License (MIT)
 # Copyright (c) 2020 UCAS
 
+cgmmetrics <- function(inputdir, outputdir, useig = FALSE, threshold =1, bthreshold = 3.9, athreshold = 10, interval = 15){
+  fileNames = list.files(inputdir)
+  freq = 1440/interval
+  fnamevec <- c()
+  sdvec <- c()
+  meanvec <- c()
+  cvvec <- c()
+  gmisvec <- c()
+  lbgivec <- c()
+  hbgivec <- c()
+  magevec <- c()
+  tirvec <- c()
+  moddvec <- c()
+  for(f in fileNames){
+    fname <- unlist(strsplit(f, split = "\\."))[1]
+    fnamevec <- append(fnamevec, fname)
+    print(paste("processing file:", f))
+    cgmtsall <- read.csv(paste(inputdir, "/", f, sep = ''),stringsAsFactors= FALSE)
+    cgmts <- cgmts[order(ymd_hm(cgmts$timestamp)),]
+    vectimestamp <- as.vector(cgmtsall$timestamp)
+    vectimestamp <- unlist(strsplit(vectimestamp,split=" "))
+    maxtimestamp <- matrix(vectimestamp,ncol=2,byrow=T)[,1]
+    cgmtsall <- dplyr::mutate(cgmtsall, timedate = maxtimestamp)
 
-#device = 0: manual format
-#device = 1: libre freestyle
-#device = 2: ipro2
-#device = 2: g6
-prepro <- function(inputdir="", outputdir="", outlierdet = TRUE, interval = 15, imputation = FALSE,
-                   immethod = "linear", maxgap = 60, compeleteday = TRUE, removeday = FALSE, device = 0, transunits = FALSE, removeflday = TRUE){
-	fileNames = list.files(inputdir)
-	for(f in fileNames){
-	  print(paste("processing file:", f))
-	  cgmts <- fformat(fpath = paste(inputdir, f, sep = ''), device = device)
-	  colnm <- colnames(cgmts)
-	  if(colnm[1] != "timestamp" || colnm[2] != "sglucose" || colnm[3] != "bglucose"){
-	    stop(paste("The format fo file '",f ,"' is incorrect and cannot be read.",sep = ""))
-	  }
-	  cgmts  = qcfun(cgmts, outlierdet, interval, imputation,immethod, maxgap, compeleteday,removeday, transunits, removeflday)
-	  write.csv(cgmts, paste(outputdir,f,sep=''),row.names = FALSE)
-	  }
+    sd <- sdall(cgmtsall = cgmtsall, useig = useig)
+    sdvec <- append(sdvec, sd)
+    mean <- meanall(cgmtsall = cgmtsall,useig = useig)
+    meanvec <- append(meanvec, mean)
+    cv <- cvall(cgmtsall = cgmtsall, useig = useig)
+    cvvec <- append(cvvec, cv)
+    gmis <-gmi(cgmtsall = cgmtsall, useig = useig)
+    gmisvec <- append(gmisvec, gmis)
+    lhbgi <-lhbgiall(cgmtsall = cgmtsall, useig = useig)
+    lbgi <- lhbgi[1]
+    lbgivec <- append(lbgivec, lbgi)
+    hbgi <- lhbgi[2]
+    hbgivec <- append(hbgivec, hbgi)
+    mage <- mageall(cgmtsall = cgmtsall, useig = useig, threshold = threshold)
+    magevec <- append(magevec, mage)
+    tir <- tirall(cgmtsall = cgmtsall, useig = useig, athreshold = athreshold, bthreshold = bthreshold)
+    tirvec <- append(tirvec, tir)
+    modd <- round(mean(modd(cgmtsall = cgmtsall, useig = useig)),2)
+    moddvec <- append(moddvec, modd)
+
+    mtcdf <- mtcgrpday(cgmtsall,useig = useig, threshold = threshold,  bthreshold = bthreshold, athreshold = athreshold, freq = freq)
+    write.csv(mtcdf, paste(outputdir,fname, "_metricsByDay.csv",sep = ""),row.names = FALSE)
+
+    summetrics <- data.frame(ID = fnamevec, SD = sdvec, Mean = meanvec, CV = cvvec, GMI = gmisvec,
+                             LBGI = lbgivec, HBGI = hbgivec, MAGE = magevec, TIR = tirvec,
+                             MODD = moddvec)
+    write.csv(summetrics, paste(outputdir,fname,"_metricsSummary.csv",sep = ""),row.names = FALSE)
+
+
+    }
+
 }
 
-fformat <- function(fpath, device = 0){
-  cgmts = ""
-  if(device == 0){
-    cgmts <- read.csv(fpath)
-    return(cgmts)
-  }else if(device == 1){
-    cgmts <- read.table(fpath, sep = "\t", skip = 2,header = TRUE)
-    cgmts <- dplyr::select(cgmts, 2,4)
-    names(cgmts) <- c("timestamp", "sglucose")
-    cgmts <-  dplyr::mutate(cgmts, timestamp = gsub("/","-", cgmts$timestamp))
-    cgmts <-  dplyr::mutate(cgmts, bglucose = NA)
-  }else if(device == 2){
-    cgmts <- read.table(fpath, sep = "\t", skip = 11,header = TRUE)
-    cgmts <- dplyr::select(cgmts, 4,10)
-    names(cgmts) <- c("timestamp", "sglucose")
-    x <- as.POSIXct(dmy_hms(cgmts$timestamp))
-    xx = paste(year(x), "-", month(x), "-", day(x), " ", hour(x), ":", minute(x), sep = "")
-    cgmts$timestamp <- xx
-    cgmts <-  dplyr::mutate(cgmts, bglucose = NA)
-  }else if(device == 3){
-    cgmts <- read.table(fpath, sep = ",", skip = 11)
-    cgmts <- select(cgmts, 2, 8)
-    names(cgmts) <- c("timestamp", "sglucose")
-    cgmts <-  dplyr::mutate(cgmts, timestamp = gsub("T"," ", cgmts$timestamp))
-    x <- as.POSIXct(ymd_hms(cgmts$timestamp))
-    xx = paste(year(x), "-", month(x), "-", day(x), " ", hour(x), ":", minute(x), sep = "")
-    cgmts$timestamp <- xx
-    cgmts <-  dplyr::mutate(cgmts, bglucose = NA)
+
+mtcgrpday <- function(cgmtsall,useig = FALSE, threshold = 1,  bthreshold = 3.9, athreshold = 10, freq = 96){
+
+  sdcol <- sdgrpbyday(cgmtsall = cgmtsall, useig = useig)
+  meancol <- meangrpbyday(cgmtsall = cgmtsall, useig = useig)
+  cvcol <- cvgrpbyday(cgmtsall = cgmtsall, useig = useig)
+  lhbgilist <- lhbgi(cgmtsall = cgmtsall, useig = useig)
+  lbgicol <- lhbgilist$lbgivec
+  hbgicol <- lhbgilist$hbgivec
+  magecol <- mage(cgmtsall = cgmtsall, useig = useig, threshold = threshold)
+  tircol <- tir(cgmtsall = cgmtsall, useig = useig,  bthreshold = bthreshold, athreshold = athreshold, freq = freq)
+  moddcol <- c(NA)
+  moddcol <- append(moddcol, modd(cgmtsall = cgmtsall, useig = useig))
+
+  metricsdf <- data.frame(Day = c(1:length(sdcol)), SD = sdcol, Mean = meancol, CV = cvcol,
+                           LBGI = lbgicol, HBGI = hbgicol, MAGE = magecol, TIR = tircol,
+                           MODD = moddcol)
+  return(metricsdf)
+}
+
+
+sdgrpbyday <- function(cgmtsall, useig = FALSE){
+  coldate <- unique(cgmtsall$timedate)
+  sdvec <- c()
+  for(d in coldate){
+    cgmts <- dplyr::filter(cgmtsall, timedate == d)
+    if(useig){
+      sdvec <- append(sdvec, round(sd(cgmts$imglucose), 2))
+    }else{
+      sdvec <- append(sdvec, round(sd(cgmts$sglucose), 2))
+    }
   }
-  return(cgmts)
+  return(sdvec)
 }
+
+sdall <- function(cgmtsall, useig = FALSE){
+  sdvec = 0
+  if(useig){
+    sdvec = round(sd(cgmtsall$imglucose), 2)
+  }else{
+    sdvec = round(sd(cgmtsall$sglucose), 2)
+  }
+  return(sdvec)
+}
+
+meangrpbyday <- function(cgmtsall, useig = FALSE){
+  coldate <- unique(cgmtsall$timedate)
+  meanvec <- c()
+  for(d in coldate){
+    cgmts <- dplyr::filter(cgmtsall, timedate == d)
+    if(useig){
+      meanvec <- append(meanvec, round(mean(cgmts$imglucose), 2))
+    }else{
+      meanvec <- append(meanvec, round(mean(cgmts$sglucose), 2))
+    }
+  }
+  return(meanvec)
+}
+
+meanall <- function(cgmtsall, useig = FALSE){
+  meanall = 0
+  if(useig){
+    meanall = round(mean(cgmtsall$imglucose), 2)
+  }else{
+    meanall = round(mean(cgmtsall$sglucose), 2)
+  }
+  return(meanall)
+}
+
+cvgrpbyday <- function(cgmtsall, useig = FALSE){
+  coldate <- unique(cgmtsall$timedate)
+  cvvec <- c()
+  for(d in coldate){
+    cgmts <- dplyr::filter(cgmtsall, timedate == d)
+    if(useig){
+      cvvec <- append(cvvec, round(sd(cgmts$imglucose)/mean(cgmts$imglucose), 2))
+    }else{
+      cvvec <- append(cvvec, round(sd(cgmts$sglucose)/mean(cgmts$sglucose), 2))
+    }
+  }
+  return(cvvec)
+}
+
+
+cvall <- function(cgmtsall, useig = FALSE){
+  cvall = 0
+  if(useig){
+    cvall = round(sd(cgmtsall$imglucose)/mean(cgmtsall$imglucose), 2)
+  }else{
+    cvall = round(sd(cgmtsall$sglucose)/mean(cgmtsall$sglucose), 2)
+  }
+  return(cvall)
+}
+
+#https://doi.org/10.2337/dc18-1581
+gmi <- function(cgmtsall, useig = FALSE){
+  mgmi = 0
+  if(useig){
+    mgmi <- 3.31 + 0.02392 * mean(cgmtsall$imglucose*18)
+  }else{
+    mgmi <- 3.31 + 0.02392 * mean(cgmtsall$sglucose*18)
+  }
+  return(round(mgmi,2))
+}
+
+#Assessment of Risk for Severe Hypoglycemia Among Adults With IDDM
+#Symmetrization of the blood glucose measurement scale and its applications
+lhbgi <- function(cgmtsall, useig = FALSE){
+  coldate <- unique(cgmtsall$timedate)
+  lbgivec <- c()
+  hbgivec <- c()
+  for(d in coldate){
+    cgmts <- dplyr::filter(cgmtsall, timedate == d)
+    if(useig){
+      fbg <- 1.794 * (log(cgmts$imglucose) ** 1.026 - 1.861)
+    }else{
+      fbg <- 1.794 * (log(cgmts$sglucose) ** 1.026 - 1.861)
+    }
+    rlbg <- fbg[fbg < 0]
+    rhbg <- fbg[fbg > 0]
+    lbgivec <- append(lbgivec, round(mean(10 * (rlbg ** 2)), 2))
+    hbgivec <- append(hbgivec, round(mean(10 * (rhbg ** 2)), 2))
+  }
+  out <- list(lbgivec=lbgivec, hbgivec=hbgivec)
+  return(out)
+}
+
+
+lhbgiall <- function(cgmtsall, useig = FALSE){
+  if(useig){
+    fbg <- 1.794 * (log(cgmtsall$imglucose) ** 1.026 - 1.861)
+  }else{
+    fbg <- 1.794 * (log(cgmtsall$sglucose) ** 1.026 - 1.861)
+  }
+  rlbg <- fbg[fbg < 0]
+  rhbg <- fbg[fbg > 0]
+  lbgivec <- round(mean(10 * (rlbg ** 2)), 2)
+  hbgivec <- round(mean(10 * (rhbg ** 2)), 2)
+  out <- c(lbgivec, hbgivec)
+  return(out)
+}
+
+#Mean Amplitude of Glycemic Excursions, a Measure of Diabetic Instability
+mage <- function(cgmtsall, useig = FALSE, threshold = 1){
+  coldate <- unique(cgmtsall$timedate)
+  magevec <- c()
+  for(d in coldate){
+    cgmts <- dplyr::filter(cgmtsall, timedate == d)
+    daysd <- 0
+    dayglucose <- c()
+    if(useig){
+      daysd <- sd(cgmts$imglucose)
+      dayglucose <- cgmts$imglucose
+    }else{
+      daysd <- sd(cgmts$sglucose)
+      dayglucose <- cgmts$sglucose
+    }
+    daysd <- threshold * daysd
+    diffglucose <- diff(dayglucose)
+
+    inorder <- TRUE
+    sflag <- TRUE
+    turnpoints <- c()
+    for(i in seq_along(diffglucose)){
+      if(diffglucose[i] ==0){
+        next
+      }
+      if(sflag){
+        if(diffglucose[i] < 0){
+          inorder <- TRUE
+        }else{
+          inorder <- FALSE
+        }
+        sflag <- FALSE
+        next
+      }
+      if(inorder){
+        if(diffglucose[i] < 0){
+          next
+        }else{
+          turnpoints <- append(turnpoints, dayglucose[i])
+          inorder <- FALSE
+        }
+
+      }else{
+        if(diffglucose[i] < 0){
+          turnpoints <- append(turnpoints, dayglucose[i])
+          inorder <- TRUE
+        }else{
+          next
+        }
+      }
+    }
+    tpdiff <- diff(turnpoints)
+    tpdiff <- tpdiff[abs(tpdiff) > daysd ]
+    if(tpdiff[1] > 0){
+      tpdiff <- tpdiff[tpdiff > 0]
+    }else if(tpdiff[1] < 0){
+      tpdiff <- tpdiff[tpdiff < 0]
+    }
+    magevec <- append(magevec, abs(round(mean(tpdiff),2)))
+  }
+
+  return(magevec)
+
+}
+mageall <- function(cgmtsall, useig = FALSE, threshold = 1){
+  mage <- 0
+  daysd <- 0
+  glucosevec <- c()
+  if(useig){
+    daysd <- sd(cgmtsall$imglucose)
+    glucosevec <- cgmtsall$imglucose
+  }else{
+    daysd <- sd(cgmtsall$sglucose)
+    glucosevec <- cgmtsall$sglucose
+  }
+  daysd <- threshold * daysd
+  diffglucose <- diff(glucosevec)
+  inorder <- TRUE
+  sflag <- TRUE
+  turnpoints <- c()
+  for(i in seq_along(diffglucose)){
+    if(diffglucose[i] ==0){
+      next
+    }
+    if(sflag){
+      if(diffglucose[i] < 0){
+        inorder <- TRUE
+      }else{
+        inorder <- FALSE
+      }
+      sflag <- FALSE
+      next
+    }
+    if(inorder){
+      if(diffglucose[i] < 0){
+        next
+      }else{
+        turnpoints <- append(turnpoints, glucosevec[i])
+        inorder <- FALSE
+      }
+    }else{
+        if(diffglucose[i] < 0){
+          turnpoints <- append(turnpoints, glucosevec[i])
+          inorder <- TRUE
+        }else{
+          next
+        }
+      }
+    }
+  tpdiff <- diff(turnpoints)
+  tpdiff <- tpdiff[abs(tpdiff) > daysd ]
+  if(tpdiff[1] > 0){
+    tpdiff <- tpdiff[tpdiff > 0]
+  }else if(tpdiff[1] < 0){
+    tpdiff <- tpdiff[tpdiff < 0]
+  }
+  mage <- abs(round(mean(tpdiff),2))
+  return(mage)
+}
+
+
+tir <- function(cgmtsall, useig = FALSE, bthreshold = 3.9, athreshold = 10, freq){
+  coldate <- unique(cgmtsall$timedate)
+  tirvec <- c()
+  for(d in coldate){
+    cgmts <- dplyr::filter(cgmtsall, timedate == d)
+    fcgmts <- ""
+    if(useig){
+      fcgmts <- dplyr::filter(cgmts, imglucose > bthreshold & imglucose < athreshold)
+    }else{
+      fcgmts <- dplyr::filter(cgmts, sglucose > bthreshold & sglucose < athreshold)
+    }
+    tirvec <- append(tirvec, round(nrow(fcgmts)/freq,2))
+  }
+  return(tirvec)
+}
+
+tirall <- function(cgmtsall, useig = FALSE, bthreshold = 3.9, athreshold = 10){
+  tirvec <- 0
+  fcgmts <- ""
+  if(useig){
+    fcgmts <- dplyr::filter(cgmtsall, imglucose > bthreshold & imglucose < athreshold)
+  }else{
+    fcgmts <- dplyr::filter(cgmtsall, sglucose > bthreshold & sglucose < athreshold)
+  }
+  tirvec <- round(nrow(fcgmts)/nrow(cgmtsall),2)
+  return(tirvec)
+}
+
+modd <- function(cgmtsall, useig = FALSE){
+  moddvec <- c()
+  coldate <- unique(cgmtsall$timedate)
+  for(i in seq_along(coldate)){
+    if(i == 1){
+      next
+    }
+    lastday <- dplyr::filter(cgmtsall,timedate == coldate[i-1])
+    cuday <- dplyr::filter(cgmtsall,timedate == coldate[i])
+    if(useig){
+      md <- abs(round(mean(cuday$imglucose - lastday$imglucose),2))
+    }else{
+      md <- abs(round(mean(cuday$sglucose - lastday$sglucose),2))
+    }
+    moddvec <- append(moddvec, md)
+  }
+  return(moddvec)
+}
+
+
+
+
+
 
